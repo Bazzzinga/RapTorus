1e376198f490ce1c0738efe6acb82269eb3b3db6
Bazzzinga <holoholo239@gmail.com>
2016-04-08 12:50:54 +0300

    Updated documentation

--- FileReader/html/search/all_4.js
+++ FileReader/html/search/all_4.js
@@ -11,6 +11,7 @@ var searchData=
   ['elementtypes_2eh',['ElementTypes.h',['../_element_types_8h.html',1,'']]],
   ['err_5fno_5ferr',['ERR_NO_ERR',['../_file_structs_8h.html#ae183556f32e14e06d49b270ca950d90aab8516ccf594649942dde7e22723cdfa9',1,'FileStructs.h']]],
   ['err_5fopen',['ERR_OPEN',['../_file_structs_8h.html#ae183556f32e14e06d49b270ca950d90aa7235954be32cd0166432c3f7a5d61bb5',1,'FileStructs.h']]],
+  ['err_5fopendb',['ERR_OPENDB',['../_file_structs_8h.html#ae183556f32e14e06d49b270ca950d90aad4a7104d7f7f4d0f7bc797cb860333df',1,'FileStructs.h']]],
   ['err_5fwrong_5ffile_5fformat',['ERR_WRONG_FILE_FORMAT',['../_file_structs_8h.html#ae183556f32e14e06d49b270ca950d90aa64318f099e2374a06887da814f26a9a7',1,'FileStructs.h']]],
   ['exec',['exec',['../class_database_agent.html#a21af2767f8078302a28c0e01f317e042',1,'DatabaseAgent']]],
   ['execbuffered',['execBuffered',['../class_database_agent.html#a947629f03edce06965a18e3a3726ade1',1,'DatabaseAgent']]],
--- FileReader/html/search/functions_4.js
+++ FileReader/html/search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['geterror',['getError',['../class_f_reader.html#abd5dae242af5ba546df2e27acf1644e9',1,'FReader::getError()'],['../class_database_handler.html#a5162cd33b92072be457be57fdcc61284',1,'DatabaseHandler::getError()']]]
+  ['geterror',['getError',['../class_f_reader.html#abd5dae242af5ba546df2e27acf1644e9',1,'FReader::getError()'],['../class_database_agent.html#a8305f1e19cb08ff19ce88a9ec3735162',1,'DatabaseAgent::getError()'],['../class_database_handler.html#a5162cd33b92072be457be57fdcc61284',1,'DatabaseHandler::getError()']]]
 ];
